3da9e298565779f2fff79a7b35e0234ab5ccc849
Gabriel Andruchevitz <gabriel.andruchevitz@gmail.com>
2025-08-14 10:51:32 -0300

    feat: implementa movimento no personagem

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class Player {
     update(){
         this.draw()
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
         if(this.position.y + this.height + this.velocity.y < canvas.height)
             this.velocity.y += gravity
         else this.velocity.y = 0
@@ -32,12 +33,49 @@ const player = new Player({
     y:0,
 })
 
+const keys = {
+    d: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0 , 0, canvas.width, canvas.height) 
-
     player.update()
-    player.draw()
+
+    player.velocity.x = 0
+    if(keys.d.pressed) player.velocity.x = 5
+        else if (keys.a.pressed) player.velocity.x = -5
 }
+
 animate()
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd':
+        keys.d.pressed = true
+        break
+        case 'a':
+        keys.a.pressed = true
+        break
+        //tecla space
+        case ' ':
+        player.velocity.y = -20
+        break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key) {
+        case 'd':
+        keys.d.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+    }
+})
