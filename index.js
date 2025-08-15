2816ebef9790cb01db2f558fd9d4149dde780b16
Gabriel Andruchevitz <gabriel.andruchevitz@gmail.com>
2025-08-15 10:04:59 -0300

    feat: implementa background

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ canvas.width = 1335
 canvas.height = 576
 const gravity = 0.5
 
+const scaledCanvas = {
+    width: canvas.width / 4,
+    height: canvas.height / 4
+}
+
+class Sprite {
+    constructor({position, imageSrc}) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc        
+    }
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+    update() {
+        this.draw()
+    }
+}
+
 class Player {
     constructor(position) {
         this.position = position
@@ -42,10 +62,23 @@ const keys = {
     }
 }
 
+const background = new Sprite({
+    position: {
+        x: 0, 
+        y: 0,
+    },
+    imageSrc: './imagens/forestbg.png'
+})
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
-    c.fillRect(0 , 0, canvas.width, canvas.height) 
+    c.fillRect(0 , 0, canvas.width, canvas.height)
+    c.save()
+    c.scale(4, 4)
+    c.translate(0,- background.image.height + scaledCanvas.height)
+    background.update() 
+    c.restore()
     player.update()
 
     player.velocity.x = 0
